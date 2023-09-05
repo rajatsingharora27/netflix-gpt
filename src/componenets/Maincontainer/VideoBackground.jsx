976d36033bdbf25aaa1a790f4105a88c44911cc9
rajat <rajatsingharora27@gmail.com>
2023-09-05 11:58:47 +0530

    added video in background

--- src/componenets/Maincontainer/VideoBackground.jsx
+++ src/componenets/Maincontainer/VideoBackground.jsx
@@ -10,11 +10,10 @@ const VideoBackground = ({ id }) => {
 
   // console.log(trailerVideoKey);
   return (
-    <div>
+    <div className="w-screen">
       <iframe
-        width="560"
-        height="315"
-        src={`https://www.youtube.com/embed/${trailerVideoKey?.key}?si=X80qTrKoolI12edV`}
+        className="w-screen aspect-video"
+        src={`https://www.youtube.com/embed/${trailerVideoKey?.key}?si=X80qTrKoolI12edV/?&autoplay=1&mute=1`}
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       ></iframe>
--- src/componenets/Maincontainer/VideoTitle.jsx
+++ src/componenets/Maincontainer/VideoTitle.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import { FaPlay, FaInfoCircle } from "react-icons/fa";
 const VideoTitle = ({ title, overview }) => {
   return (
-    <div className="pt-36  px-12 border-2 border-yellow-300 border-solid flex flex-col justify-start">
+    <div className="absolute bg-gradient-to-r from-black text-white w-screen aspect-video pt-[20%]  px-24 flex flex-col justify-start">
       <h1 className="font-bold text-3xl">{title}</h1>
-      <p className="w-11/12 text-lg py-4 lg:w-1/2">{overview}</p>
+      <p className="w-11/12 text-lg py-4  lg:w-1/2 ">{overview}</p>
       <div className="flex justify-start items-center gap-x-2">
-        <button className="bg-white text-black text-lg font-bold px-10 p-4 flex items-center justify-center gap-x-2 bg-opacity-50 rounded-lg border-2 shadow-lg">
+        <button className="bg-white text-black text-lg font-bold px-10 p-4 flex items-center justify-center gap-x-2 bg-opacity-50 rounded-lg border-2 shadow-lg hover:opacity-80">
           <FaPlay /> <span>Play</span>
         </button>
-        <button className="bg-gray-500 text-black text-lg font-bold px-8 p-4 flex items-center justify-center gap-x-2 bg-opacity-50 rounded-lg border-2 shadow-lg">
+        <button className="bg-gray-500 text-white text-lg font-sm px-8 p-4 flex items-center justify-center gap-x-2 bg-opacity-50 rounded-lg border-2 shadow-lg hover:bg-slate-400">
           <FaInfoCircle />
           <span>More Info</span>
         </button>
