c4763d676289ddcd368edcc90937f4179c82e7d0
rajat <rajatsingharora27@gmail.com>
2023-09-06 00:07:42 +0530

    resolved deployment issues

--- src/utils/constants/apiConstants.js
+++ src/utils/constants/apiConstants.js
@@ -1,12 +1,16 @@
-export const URL = process.env.REACT_APP_MOVIE_LIST_URL;
-export const POPLUAR_MOVIE_URL = process.env.REACT_APP_POPULAR_MOVIE;
-export const TRENDING = process.env.REACT_APP_TENDING;
+export const URL =
+  "https://api.themoviedb.org/3/discover/movie?include_adult=false&page=1&sort_by=popularity.desc";
+export const POPLUAR_MOVIE_URL =
+  "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1";
+export const TRENDING =
+  "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1";
 // console.log(URL);
 export const API_OPTIONS = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization: `Bearer ${process.env.REACT_APP_TOKEN}`,
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMDIxNDU1ODJiN2IwYmU2YzQwYTg0NmMzYWNiMDQxZSIsInN1YiI6IjYyNzY2YjhmOTU2NjU4MTFlYTdlNmNlNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0W0qZaV-VBVuAwtMPCqy-ldZl19uL_zmha-ZiDRbj6g",
   },
 };
 
@@ -14,3 +18,5 @@ export const API_OPTIONS = {
 //   .then(res => res.json())
 //   .then(json => console.log(json))
 //   .catch(err => console.error('error:' + err));
+
+// REACT_APP_MOVIE_TRAILER = "https://api.themoviedb.org/3/movie/";
